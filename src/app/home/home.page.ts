bf78feb158c546f90f04acc465e128eb053baaae
David Valchář <d_valchar@utb.cz>
2025-11-24 21:21:24 +0100

    connected REST API and made html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
 
 @Component({
   selector: 'app-home',
@@ -8,6 +13,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  weatherTemp : any
+  todayDate = new Date()
+  cityName : any
+  weatherIcon : any
+  weatherDetails : any
 
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
+
+  loadData(){
+    this.httpClient.get<any>(`${API_URL}/weather?q=${"Zlín"}&appid=${API_KEY}`).subscribe(results =>{
+      console.log(results)
+      this.weatherTemp = results['main']
+      this.cityName = results['name']
+      console.log(this.weatherTemp)
+      this.weatherDetails = results['weather'][0]
+      console.log(this.weatherDetails)
+      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`
+
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '51e48c58ba14bd2d1049f79c572f8d53',
+  API_URL: 'https://api.openweathermap.org/data/2.5'
 };
 
 /*
